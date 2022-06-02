061563f5492bc579a6fccdc44e6d4569b499b999
Sagar Gunjal <sagar.gunjal@morningstar.com>
2022-06-02 16:49:37 +0530

    day 02-05-2022

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+// importing express library to create server
+const express = require("express") 
+
+const app = express()
+
+app.get("/user", (req,res)=>{
+    res.send( "First API call")
+})
+
+const port = 5000
+app.listen(5000 , (req , res)=>{
+    console.log(`Starting Application on port : http://localhost:${port}`)
+})
