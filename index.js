e32b98f154e3501188557da5bbd2e55775b5dec1
Sagar Gunjal <sagar.gunjal@morningstar.com>
2022-06-03 12:45:00 +0530

    updates

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// importing express library to create server
-const express = require("express") 
+//Importing express library to create server
+const express = require("express")
 
 const mongoose = require("mongoose")
 
 
+
 const app = express()
 
+app.get("/user", (req, res) =>{
+    res.send("Fetch API Call")
+})
+const port = 5500
 
-app.get("/user", (req,res)=>{
-    res.send( "First API call")
+app.listen(port,(req,res) =>{
+    console.log(`Starting application on port : http://localhost: ${port}`)
 })
 
-const port = 5000
-app.listen(5000 , (req , res)=>{
-    console.log(`Starting Application on port : http://localhost:${port}`)
-})
-
-const mongodbur1 ="mongodb+srv://sagargunjal2955:sagar@cluster0.7a5skea.mongodb.net/?retryWrites=true&w=majority" 
-mongoose.connect(mongodbur1)
+const mongodburl = "mongodb+srv://sagargunjal2955:sagar2955@cluster0.7a5skea.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongodburl)
 .then(()=>{
-    console.log("DB connected")
+    console.log("DB Connected!!")
 })
 .catch((err)=>{
     console.log(err)
-    console.log("not connected")
 })
+
